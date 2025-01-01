d3eefb7601b1640a21191ff7dfb0801b98d4957d
rxasif07 <rxasif31@gmail.com>
2025-01-01 17:39:38 +0600

    home change

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2 className="text-4xl text-white mt-[100px]">this is home page </h2>
+      <h2 className="text-4xl text-white mt-[100px]">home page </h2>
     </div>
   );
 }
